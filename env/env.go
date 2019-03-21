de32959ae9d598c9623b04f484ad8fe9b438ce4b
Henrique <rick.corilaco@gmail.com>
2019-03-21 08:18:35 -0400

    [env] lib for managing environment variables

---
+++ env/env.go
@@ -0,0 +1,75 @@
+// Package env is a lib for managing environment variables, it
+// makes it easy to add and fetch values
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+// Set adds a value in the environment variables
+func Set(key, value string) error {
+	return os.Setenv(key, value)
+}
+
+// MustSet adds a value in the environment variables
+// and generates a panic in case of error
+func MustSet(key, value string) {
+	err := Set(key, value)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Get search for a value between the environment variables
+func Get(key string) string {
+	return os.Getenv(key)
+}
+
+// GetInt search for a value between the environment
+// variables and convert to int
+func GetInt(key string) (int, error) {
+	return strconv.Atoi(Get(key))
+}
+
+// MustGetInt search for a value between the environment
+// variables, convert to int and generates a panic in case of error
+func MustGetInt(key string) int {
+	value, err := GetInt(key)
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+
+// GetInt64 search for a value between the environment variables
+// and convert to int64
+func GetInt64(key string) (int64, error) {
+	return strconv.ParseInt(Get(key), 10, 64)
+}
+
+// MustGetInt64 search for a value between the environment
+// variables, convert to int64 and generates a panic in case of error
+func MustGetInt64(key string) int64 {
+	value, err := GetInt64(key)
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+
+// GetFloat64 earch for a value between the environment variables
+// and convert to float64
+func GetFloat64(key string) (float64, error) {
+	return strconv.ParseFloat(Get(key), 64)
+}
+
+// MustGetFloat64 search for a value between the environment
+// variables, convert to float64 and generates a panic in case of error
+func MustGetFloat64(key string) float64 {
+	value, err := GetFloat64(key)
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
