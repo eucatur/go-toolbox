e0761429d4e8f78d164b20795c33d233e93efc96
André Betiolo <andrebetiolo@gmail.com>
2019-05-08 14:43:35 -0400

    Adicionado Sha1Base64

--- crypt/sha1.go
+++ crypt/sha1.go
@@ -2,6 +2,7 @@ package crypt
 
 import (
 	cryptosha1 "crypto/sha1"
+	"encoding/base64"
 	"encoding/hex"
 )
 
@@ -13,4 +14,14 @@ func Sha1(content string) string {
 	h := cryptosha1.New()
 	h.Write([]byte(content))
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+// Sha1Base64 gera um hash SHA1 com base64 para o parâmetro content.
+//
+//  hash := Sha1Base64("texto")
+//
+func Sha1Base64(content string) string {
+	h := cryptosha1.New()
+	h.Write([]byte(content))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
