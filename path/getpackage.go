1b1b136d87f0e853b5fc08e07358803d1eb84181
Henrique Guimarães Corilaço <rick.corilaco@gmail.com>
2019-08-27 17:41:11 -0400

    [path] Implementing the GetRelativePath function

--- path/getpackage.go
+++ path/getpackage.go
@@ -1,7 +1,10 @@
 package path
 
 import (
+	"errors"
 	"os"
+	"path/filepath"
+	"runtime"
 	"strings"
 )
 
@@ -13,4 +16,26 @@ func GetPackage(repository string) (packagePath string) {
 	path := strings.Split(dir, repository)
 	p := strings.Split(path[1], "/")
 	return path[0] + repository + p[0] + "/"
+}
+
+// GetRelativePath returns the path to the file that called this function without the project root path.
+func GetRelativePath() (relativePath string, err error) {
+	rootPath, err := os.Getwd()
+	if err != nil {
+		return
+	}
+
+	_, fullPathFile, _, ok := runtime.Caller(0)
+	if !ok {
+		err = errors.New("unable to identify file path")
+		return
+	}
+
+	relativePathFile, err := filepath.Rel(rootPath, fullPathFile)
+	if err != nil {
+		return
+	}
+
+	relativePath = filepath.Dir(relativePathFile)
+	return
 }
