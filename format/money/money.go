19bd6262491fff40fc521ccfce1892e4fd153725
Darllan <darllanjptissei@gmail.com>
2019-09-18 16:31:28 -0400

    Adicionado função para arredondamento de valores float

--- format/money/money.go
+++ format/money/money.go
@@ -3,6 +3,7 @@ package money
 import (
 	"bytes"
 	"fmt"
+	"math"
 )
 
 func Reais(valor int64) string {
@@ -36,4 +37,29 @@ func Reais(valor int64) string {
 	buff.WriteString(fmt.Sprintf("%02d", centavos))
 
 	return buff.String()
+}
+
+// Round faz o arredondamento das casas decimais do parâmetro 'value' de
+// acordo com a quantidade de casas informado em 'precision'.
+//
+// Exemplos:
+//
+//  dinheiro := Round(150.141, 2)  // 150.14
+//  dinheiro := Round(150.145, 2)  // 150.15
+//  dinheiro := Round(150.146, 2)  // 150.15
+//
+func Round(value float64, precision int) float64 {
+	var round float64
+
+	pow := math.Pow(10, float64(precision))
+	digit := pow * value
+	_, div := math.Modf(digit)
+
+	if div >= .5 {
+		round = math.Ceil(digit)
+	} else {
+		round = math.Floor(digit)
+	}
+
+	return round / pow
 }
