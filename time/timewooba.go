04dce94f1c8f7be5a67874463360115c22bb9d92
Henrique Guimarães Corilaço <rick.corilaco@gmail.com>
2020-03-23 08:09:00 -0400

    [time] woobatime type

---
+++ time/timewooba.go
@@ -0,0 +1,94 @@
+package time
+
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// /Date(1584617088596-0300)/
+
+// Timewooba ...
+type Timewooba struct{ time.Time }
+
+// MarshalJSON ...
+func (t Timewooba) MarshalJSON() (bytes []byte, err error) {
+	return []byte(fmt.Sprintf(`"%s"`, t.String())), nil
+}
+
+// UnmarshalJSON ...
+func (t *Timewooba) UnmarshalJSON(bytes []byte) (err error) {
+	value, err := strconv.Unquote(string(bytes))
+	if err != nil {
+		return
+	}
+
+	timewooba, err := ParseTimewooba(value)
+	if err != nil {
+		return
+	}
+
+	t = &timewooba
+	return
+}
+
+// Value ...
+func (t Timewooba) Value() (value driver.Value, err error) {
+	if t.IsZero() {
+		return
+	}
+
+	return t.String(), nil
+}
+
+// Scan ...
+func (t *Timewooba) Scan(value interface{}) (err error) {
+	switch v := value.(type) {
+	case Timewooba:
+		t.Time = v.Time
+	case time.Time:
+		t.Time = v
+	case string:
+		if v != "" {
+			var timewooba Timewooba
+
+			timewooba, err = ParseTimewooba(v)
+			if err != nil {
+				return
+			}
+
+			t.Time = timewooba.Time
+		}
+	}
+
+	return
+}
+
+// String ...
+func (t Timewooba) String() string {
+	if t.IsZero() {
+		return ""
+	}
+	return fmt.Sprintf("/Date(%.13s%s)/", strconv.FormatInt(t.UnixNano(), 10), t.Format("-0700"))
+}
+
+// ParseTimewooba ...
+func ParseTimewooba(value string) (timewooba Timewooba, err error) {
+	if len(value) != 26 {
+		err = errors.New("Invalid date")
+		return
+	}
+
+	nsecStr := value[6:19] + strings.Repeat("0", 6)
+
+	nsec, err := strconv.ParseInt(nsecStr, 10, 64)
+	if err != nil {
+		return
+	}
+
+	timewooba = Timewooba{time.Unix(0, nsec)}
+	return
+}
---
+++ time/timewooba_test.go
@@ -0,0 +1,100 @@
+package time
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	rfc3339Date = "2020-03-20T14:02:50-04:00"
+	woobaDate   = "/Date(1584727370000-0400)/"
+)
+
+func TestTimewoobaUnmarshal(t *testing.T) {
+	assert := assert.New(t)
+
+	date := Timewooba{}
+	date.Time, _ = time.Parse(time.RFC3339, rfc3339Date)
+
+	st := struct {
+		Date Timewooba `json:"date" db:"data"`
+	}{
+		Date: date,
+	}
+
+	expectedJSON := `{"date":"` + woobaDate + `"}`
+
+	actualJSON, err := json.Marshal(st)
+	assert.Equal(nil, err)
+	assert.Equal(expectedJSON, string(actualJSON))
+}
+
+func TestTimewoobaMarshal(t *testing.T) {
+	assert := assert.New(t)
+
+	date := Timewooba{}
+	date.Time, _ = time.Parse(time.RFC3339, rfc3339Date)
+
+	st := struct {
+		Date Timewooba `json:"date" db:"date"`
+	}{
+		Date: date,
+	}
+
+	expectedJSON := `{"date":"` + woobaDate + `"}`
+
+	actualJSON, err := json.Marshal(st)
+	assert.Equal(nil, err)
+	assert.Equal(expectedJSON, string(actualJSON))
+
+	stActual := st
+
+	err = json.Unmarshal(actualJSON, &stActual)
+	assert.Equal(nil, err)
+	assert.Equal(woobaDate, stActual.Date.String())
+}
+
+func TestTimewoobaScan(t *testing.T) {
+	assert := assert.New(t)
+
+	dateActual := Timewooba{}
+
+	err := dateActual.Scan(woobaDate)
+	assert.Equal(nil, err)
+	assert.Equal(woobaDate, dateActual.String())
+
+	timewooba, err := ParseTimewooba(woobaDate)
+	assert.Equal(nil, err)
+
+	dateActual = Timewooba{}
+
+	err = dateActual.Scan(timewooba.Time)
+	assert.Equal(nil, err)
+	assert.Equal(woobaDate, dateActual.String())
+
+	dataTimewooba := Timewooba{Time: timewooba.Time}
+	dateActual = Timewooba{}
+
+	err = dateActual.Scan(dataTimewooba)
+	assert.Equal(nil, err)
+	assert.Equal(woobaDate, dateActual.String())
+}
+
+func TestTimewoobaValue(t *testing.T) {
+	assert := assert.New(t)
+
+	timewooba, err := ParseTimewooba(woobaDate)
+	assert.Equal(nil, err)
+
+	dateActual := Timewooba{Time: timewooba.Time}
+
+	value, err := dateActual.Value()
+	assert.Equal(nil, err)
+
+	valueString, ok := value.(string)
+	assert.Equal(true, ok)
+	assert.Equal(woobaDate, valueString)
+}
