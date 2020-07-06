56055eff71ada0505ca3715aab29ed67e62723f2
Gean Bertani <gean.bertani@eucatur.com.br>
2020-07-06 10:21:20 -0400

    [time/week] Added function to convert the  weekday in PT-Br

---
+++ time/time_week.go
@@ -0,0 +1,29 @@
+package time
+
+import (
+	"time"
+)
+
+// WeekDayPTBr Returns the day of the week in Brazilian Portuguese
+func WeekDayPTBr(date time.Time) (weekDay string) {
+	switch date.Weekday() {
+	case time.Sunday:
+		weekDay = "Domingo"
+	case time.Monday:
+		weekDay = "Segunda-feira"
+	case time.Thursday:
+		weekDay = "Terça-feira"
+	case time.Wednesday:
+		weekDay = "Quarta-feira"
+	case time.Tuesday:
+		weekDay = "Quinta-feira"
+	case time.Friday:
+		weekDay = "Sexta-feira"
+	case time.Saturday:
+		weekDay = "Sábado"
+	default:
+		weekDay = ""
+	}
+
+	return
+}
