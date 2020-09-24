c321c164b3ca870f4e7b8aa02bc90f996b3a29d5
Henrique Guimarães Corilaço <rick.corilaco@gmail.com>
2020-09-24 11:25:38 -0400

    [time] parse time EUA and addDate

--- time/time_eua.go
+++ time/time_eua.go
@@ -53,3 +53,16 @@ func (t TimeEUA) String() string {
 	}
 	return t.Format(TimeEUALayout)
 }
+
+// AddDate returns the time corresponding to adding the given number of years, months, and days to t. For example, AddDate(-1, 2, 3) applied to January 1, 2011 returns March 4, 2010.
+// AddDate normalizes its result in the same way that Date does, so, for example, adding one month to October 31 yields December 1, the normalized form for November 31.
+func (t TimeEUA) AddDate(years int, months int, days int) TimeEUA {
+	t.Time = t.Time.AddDate(years, months, days)
+	return t
+}
+
+// ParseTimeEUA parses a formatted string and returns the time value it represents.
+func ParseTimeEUA(value string) (timeEUA TimeEUA, err error) {
+	timeEUA.Time, err = time.Parse(TimeEUALayout, value)
+	return
+}
