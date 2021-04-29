1c91e966f682681225dae3b8ff81b7f88aca1445
fabiojiparana <fabiojiparana@gmail.com>
2021-04-29 16:55:16 -0400

    adicionado apm conection

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"time"
@@ -38,10 +39,19 @@ type dbConfig struct {
 }
 
 var connections = map[string]*sqlx.DB{}
+var connectionsCtx = map[string]*sql.DB{}
+
 var _config = dbConfig{}
 
 func get(filePath string) (*sqlx.DB, error) {
 	if db, found := connections[filePath]; found {
+		return db, nil
+	}
+	return nil, fmt.Errorf(`Error database not found. File path: %s`, filePath)
+}
+
+func getCtx(filePath string) (*sql.DB, error) {
+	if db, found := connectionsCtx[filePath]; found {
 		return db, nil
 	}
 	return nil, fmt.Errorf(`Error database not found. File path: %s`, filePath)
@@ -65,30 +75,13 @@ func connect(config dbConfig) (*sqlx.DB, error) {
 		))
 
 	case "mysql":
-		if config.Context == nil {
-			db, err = sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
-				config.User,
-				config.Password,
-				config.Host,
-				config.Port,
-				config.DataBase,
-			))
-		} else {
-			db, err := apmsql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
-				config.User,
-				config.Password,
-				config.Host,
-				config.Port,
-				config.DataBase,
-			))
-			if err != nil {
-				return nil, err
-			}
-			if err = db.PingContext(*config.Context); err != nil {
-				db.Close()
-				return nil, err
-			}
-		}
+		db, err = sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
+			config.User,
+			config.Password,
+			config.Host,
+			config.Port,
+			config.DataBase,
+		))
 
 	case "sqlite3":
 		db, err = sqlx.Connect("sqlite3", config.PathToDBFile)
@@ -111,11 +104,78 @@ func connect(config dbConfig) (*sqlx.DB, error) {
 	return db, nil
 }
 
+func connectCtx(config dbConfig) (*sql.DB, error) {
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	switch config.Type {
+	case "mysql":
+		if config.Context == nil {
+			return nil, err
+		}
+
+		db, err = apmsql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
+			config.User,
+			config.Password,
+			config.Host,
+			config.Port,
+			config.DataBase,
+		))
+		if err != nil {
+			return nil, err
+		}
+		if err = db.PingContext(*config.Context); err != nil {
+			db.Close()
+			return nil, err
+		}
+	default:
+		return nil, errors.New("Error database type is not supported")
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf(`Error connecting to database of type "%s" because of: %s`, config.Type, err.Error())
+	}
+
+	maxLifeTime := time.Duration(config.MaxLifeTime)
+
+	db.SetMaxOpenConns(config.MaxOpenConnection)
+	db.SetMaxIdleConns(config.MaxIdleConnection)
+	db.SetConnMaxLifetime(maxLifeTime * time.Minute)
+
+	connectionsCtx[config.FilePath] = db
+	return db, nil
+}
+
 // GetByFile Create a database connection through
 // the path of a file
-func GetByFile(ctx *context.Context, filePath string) (*sqlx.DB, error) {
+func GetByFile(filePath string) (*sqlx.DB, error) {
 
 	if db, err := get(filePath); err == nil {
+		return db, nil
+	}
+
+	var (
+		config dbConfig
+		err    error
+	)
+
+	if err = json.UnmarshalFile(filePath, &config); err != nil {
+		return nil, err
+	}
+
+	config.FilePath = filePath
+	_config = config
+
+	return connect(config)
+}
+
+// GetByFile Create a database connection through
+// the path of a file
+func GetByFileCtx(ctx *context.Context, filePath string) (*sql.DB, error) {
+
+	if db, err := getCtx(filePath); err == nil {
 		return db, nil
 	}
 
@@ -132,13 +192,14 @@ func GetByFile(ctx *context.Context, filePath string) (*sqlx.DB, error) {
 	config.Context = ctx
 	_config = config
 
-	return connect(config)
+	return connectCtx(config)
+
 }
 
 // MustGetByFile Create a database connection through
 // the path of a file and generates a panic in case of error
 func MustGetByFile(filePath string) *sqlx.DB {
-	db, err := GetByFile(nil, filePath)
+	db, err := GetByFile(filePath)
 	if err != nil {
 		panic(err)
 	}
@@ -147,8 +208,8 @@ func MustGetByFile(filePath string) *sqlx.DB {
 
 // MustGetByFile Create a database connection through
 // the path of a file and generates a panic in case of error
-func MustGetByFileCtx(ctx context.Context, filePath string) *sqlx.DB {
-	db, err := GetByFile(&ctx, filePath)
+func MustGetByFileCtx(ctx context.Context, filePath string) *sql.DB {
+	db, err := GetByFileCtx(&ctx, filePath)
 	if err != nil {
 		panic(err)
 	}
